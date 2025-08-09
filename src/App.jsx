d91d6ecf1be927f96180360e1479097cb73d0f36
Mons <ah.mounsef@gmail.com>
2025-08-10 00:19:11 +0200

    add the search input to the Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,21 @@
-import React from 'react'
+import React, {useState} from 'react'
+import Search from "./components/Search.jsx";
 
 const App = () => {
+    const [searchTerm, setSearchTerm] = useState('')
     return (
         <main>
-            <div className="pattern"/>
+            <div className="pattern" />
                 <div className="wrapper">
                     <header >
                         <img src= "./hero.png" alt= "hero" />
-                        <h1>Find <span className="text-gradient">Movies</span> you'll enjoy </h1>
+                        <h1>Find <span className="text-gradient">Movies</span> You'll Enjoy </h1>
                     </header>
 
-                    <p>Search</p>
+                    <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
 
                 </div>
+
         </main>
     )
 }
