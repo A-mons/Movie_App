33cd0c87fd913a3baa007bc3b53d8afa7012a560
Mons <ah.mounsef@gmail.com>
2025-08-09 04:58:16 +0200

    add header

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,18 @@ import React from 'react'
 
 const App = () => {
     return (
-        <div>App</div>
+        <main>
+            <div className="pattern"/>
+                <div className="wrapper">
+                    <header >
+                        <img src= "./hero.png" alt= "hero" />
+                        <h1>Find <span className="text-gradient">Movies</span> you'll enjoy </h1>
+                    </header>
+
+                    <p>Search</p>
+
+                </div>
+        </main>
     )
 }
 export default App
